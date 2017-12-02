8ed048e155ac1c728e187797a2e77920bd35dc82
sathv <sathviknallamalli@gmail.com>
2017-12-01 23:45:16 -0800

    web service connection

--- app/src/main/java/com/example/sathv/olympiahighschoollibrary/Login.java
+++ app/src/main/java/com/example/sathv/olympiahighschoollibrary/Login.java
@@ -1,21 +1,27 @@
 package com.example.sathv.olympiahighschoollibrary;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A login screen that offers login via email/password.
@@ -23,185 +29,100 @@ import java.sql.SQLException;
 public class Login extends Activity {
 
     // UI references.
-    private EditText mUsername;
-    private EditText mPasswordView;
+    private EditText usernameField;
+    private EditText passwordField;
     private View mLoginFormView;
 
-    static String name;
+    Connection connection;
 
-   private Button buttons;
+    ProgressBar pb;
 
-    String ip, database, un, pd;
+    static String name = "temporary for now";
+
+    private Button buttons;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mUsername = (EditText) findViewById(R.id.username);
-        mPasswordView = (EditText) findViewById(R.id.password);
+        usernameField = (EditText) findViewById(R.id.username);
+        passwordField = (EditText) findViewById(R.id.password);
 
         buttons = (Button) findViewById(R.id.signUp);
 
-        /*buttons.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(Login.this,
-                        SignUp.class);
-                startActivity(myIntent);
-            }
-        });*/
 
-
-        mPasswordView.setOnKeyListener(new View.OnKeyListener() {
+        passwordField.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((keyCode == KeyEvent.KEYCODE_ENTER)) {
 
-                   /* Connection con = createConnection(un, pd, database, ip);
-
-                    if (con == null) {
-                        Toast.makeText(Login.this, "FAIL login", Toast.LENGTH_LONG).show();
-                    }
-
-                    try {
-                        String query = "select * from UserInfo where Username=? and Password=?";
-                        PreparedStatement pst = con.prepareStatement(query);
-                        pst.setString(1, mUsername.getText().toString());
-                        pst.setString(2, mPasswordView.getText().toString());
-                        ResultSet rs = pst.executeQuery();
-                        int count = 0;
-
-                        while (rs.next()) {
-                            if (count == 1) {
-                                Toast.makeText(Login.this, "Login Success", Toast.LENGTH_LONG).show();
-                                con.close();
-                                Intent intent = new Intent(getApplicationContext(), Home.class);
-                                startActivity(intent);
-
-                            } else {
-                                Toast.makeText(Login.this, "Login Invalid", Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }*/
-
-                    name = mUsername.getText().toString();
-
-                    if(mPasswordView.getText().toString().isEmpty() || name.isEmpty()) {
-                        Toast.makeText(Login.this, "Missing field", Toast.LENGTH_LONG).show();
-                    }
-                    else{
-                        Intent intent = new Intent(getApplicationContext(), Activities.class);
-                        startActivity(intent);
-                    }
-
+                    Login();
 
                     return true;
-
                 }
                 return false;
             }
+
         });
 
-        ip = "127.0.0.1";
-        database = "OlyHighLibrary";
-        un = "sa";
-        pd = "Saibaba";
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.logIn);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button login = (Button) findViewById(R.id.logIn);
+        login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Connection con = createConnection(un, pd, database, ip);
+                Login();
 
-                if (con == null) {
-                    Toast.makeText(Login.this, "FAIL login", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Toast.makeText(Login.this, "GOOD login", Toast.LENGTH_LONG).show();
-                }
-
-                try {
-
-                    String query = "select * from UserInfo where Username=? and Password=?";
-                    PreparedStatement pst = con.prepareStatement(query);
-                    pst.setString(1, mUsername.getText().toString());
-                    pst.setString(2, mPasswordView.getText().toString());
-                    ResultSet rs = pst.executeQuery();
-                    int count = 0;
-
-                    while (rs.next()) {
-                        if (count == 1) {
-                            Toast.makeText(Login.this, "Login Success", Toast.LENGTH_LONG).show();
-                            con.close();
-                            Intent intent = new Intent(getApplicationContext(), Home.class);
-                            startActivity(intent);
-
-                        } else {
-                            Toast.makeText(Login.this, "Login Invalid", Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }*/
-
-                name = mUsername.getText().toString();
-
-                if(mPasswordView.getText().toString().isEmpty() || name.isEmpty()) {
-                    Toast.makeText(Login.this, "Missing field", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Intent intent = new Intent(getApplicationContext(), Activities.class);
-                    startActivity(intent);
-                }
             }
         });
 
     }
 
-    public class DoLogin extends AsyncTask<String, String, String>{
-
-       // String z = ""
-
-        @Override
-        protected String doInBackground(String... strings) {
-            return null;
-        }
-    }
-
-
-    @SuppressLint("NewApi")
-    public Connection createConnection(String user, String password, String database, String server) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        Connection connection = null;
-        String ConnectionURL = "";
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://" + "localhost" + ";"
-                    + "databaseName=" + database + ";user=" + user + ";password="
-                    + password + ";instance=SQLEXPRESS";
-            connection = DriverManager.getConnection(ConnectionURL);
-            Toast.makeText(Login.this, "Connection Success", Toast.LENGTH_LONG).show();
-
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    public void action(View view ) {
+    public void action(View view) {
         Intent dashboard = new Intent(this, SignUp.class);
         startActivity(dashboard);
 
+    }
+
+    public void Login() {
+        String url = "https://sathviknallamalli.000webhostapp.com/loginwebhost.php";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.trim().equals("success")) {
+                    Toast.makeText(getApplicationContext(), "Login success!", Toast.LENGTH_SHORT).show();
+
+                    Intent activities = new Intent(getApplicationContext(), Activities.class);
+                    startActivity(activities);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "error" + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+
+                params.put("username", usernameField.getText().toString().trim());
+                params.put("password", passwordField.getText().toString().trim());
+
+
+                return params;
+
+            }
+        };
+
+        requestQueue.add(stringRequest);
 
     }
+
 
 
 }
--- app/src/main/java/com/example/sathv/olympiahighschoollibrary/SignUp.java
+++ app/src/main/java/com/example/sathv/olympiahighschoollibrary/SignUp.java
@@ -5,9 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
 public class SignUp extends Activity {
 
     EditText username;
@@ -32,26 +29,6 @@ public class SignUp extends Activity {
     }
 
     public void registerToDatabase(View view) {
-        Login l = new Login();
-        //String user, String password, String database, String server
-        Connection con = l.createConnection("sa", "Saibaba", "OlyHighLibrary", "127.0.0.1");
-
-        try {
-            String query = "insert into UserInfo (Username,Password,FirstName,LastName,Email,Pincode) values (?, ?, ?, ?, ?, ?)";
-
-            PreparedStatement pst = con.prepareStatement(query);
-            pst.setString(1, username.getText().toString());
-            pst.setString(2, password.getText().toString());
-            pst.setString(3, firstName.getText().toString());
-            pst.setString(4, lastName.getText().toString());
-            pst.setString(5, email.getText().toString());
-            pst.setString(6, code.getText().toString());
-
-            pst.execute();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
